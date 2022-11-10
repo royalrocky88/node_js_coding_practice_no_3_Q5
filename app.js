dcd43a19c4b270ef2d284251aa4ebeb34b0929b0
Rocky Ranjan <royalrocky88@gmail.com>
2022-11-10 09:03:46 +0000

    Date after 100 Days from Today

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const addDays = require("date-fns/addDays");
+
+app.get("/", (request, response) => {
+  const date = addDays(new Date(), 100);
+
+  response.send(
+    `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+  );
+});
+
+app.listen(3000);
+
+module.exports = app;
